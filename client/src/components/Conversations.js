385b55407616360b542bbc54f03997dabc6d08d7
James Sgarella <Jimbo8702@gmail.com>
2021-12-29 11:13:39 -0500

    conversations show up when created with contact names

--- client/src/components/Conversations.js
+++ client/src/components/Conversations.js
@@ -1,5 +1,16 @@
 import React from "react";
-
+import { ListGroup } from "react-bootstrap";
+import { useConversations } from "../context/ConversationsProvider";
 export default function Conversations() {
-  return <div>conversations</div>;
+  const { conversations } = useConversations();
+
+  return (
+    <ListGroup variant="flush">
+      {conversations.map((conversation, index) => (
+        <ListGroup.Item key={index}>
+          {conversation.recipients.map((r) => r.name).join(", ")}
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
+  );
 }
