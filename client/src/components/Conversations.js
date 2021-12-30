0bab847cc2834466a751b68e87e9d0b77e0cf045
James Sgarella <Jimbo8702@gmail.com>
2021-12-30 10:40:53 -0500

    conversations are selectable

--- client/src/components/Conversations.js
+++ client/src/components/Conversations.js
@@ -2,12 +2,19 @@ import React from "react";
 import { ListGroup } from "react-bootstrap";
 import { useConversations } from "../context/ConversationsProvider";
 export default function Conversations() {
-  const { conversations } = useConversations();
+  const { conversations, selectConversationIndex } = useConversations();
 
   return (
     <ListGroup variant="flush">
       {conversations.map((conversation, index) => (
-        <ListGroup.Item key={index}>
+        <ListGroup.Item
+          key={index}
+          action
+          onClick={() => {
+            selectConversationIndex(index);
+          }}
+          active={conversation.selected}
+        >
           {conversation.recipients.map((r) => r.name).join(", ")}
         </ListGroup.Item>
       ))}
